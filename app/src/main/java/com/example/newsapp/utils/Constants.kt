01996005f750cf5b6f6bc81a7b6a0de4ba346a63
koryt <korytov_1997@mail.ru>
2023-04-09 11:03:35 +0800

    Lession-3

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "8d7d456d40044b01aafbe9b80cc26ece"
+    }
+}
