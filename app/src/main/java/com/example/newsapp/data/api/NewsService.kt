75eafe56242dcb50ef1763c6f861d66c1bf623a0
koryt <korytov_1997@mail.ru>
2023-04-09 12:55:34 +0800

    Lession-4

--- app/src/main/java/com/example/newsapp/data/api/NewsService.kt
+++ app/src/main/java/com/example/newsapp/data/api/NewsService.kt
@@ -1,23 +1,23 @@
 package com.example.newsapp.data.api
+import retrofit2.Response
+import com.example.newsapp.models.NewsResponse
 import com.example.newsapp.utils.Constants.Companion.API_KEY
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-class NewsService {
+interface NewsService {
 
     @GET("/v2/everything")
     suspend fun getEverything(
         @Query("q") query: String,
         @Query("page") page: Int = 1,
         @Query("apiKey") apiKey: String = API_KEY
-    ) {
-    }
+    ) : Response<NewsResponse>
 
+    @GET("/v2/top-headlines")
     suspend fun  getHeadLines(
         @Query("country") countryCode: String = "ru",
         @Query("page") page: Int = 1,
         @Query("apiKey") apiKey: String = API_KEY
-    ) {
-    }
-
+    ): Response<NewsResponse>
 }
